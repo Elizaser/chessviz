3f270a65804a28ca07f8781254e995cfe35a5de8
elizaser <liza.gluhowrowa@gmail.com>
2020-03-06 07:15:34 +0700

    Code with .txt file

---
+++ main.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <string.h>
+const int N = 8;
+
+int main(int argc, char* argv[]) {
+  char filename[10];
+  FILE* file;
+  int i, j;
+  char a[N][N + 1];
+
+  if (argc > 1) {
+    strcpy(filename, argv[1]);
+  }
+
+  file = fopen(filename, "r");
+  if (!file) printf("Error\n");
+
+  fread(a, sizeof(a), 1, file);
+
+  fclose(file);
+
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N + 1; j++) printf("%c", a[i][j]);
+  }
+}
