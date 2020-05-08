6105a8fba994a8d613abe412b374b9f0de9b7443
elizaser <liza.gluhowrowa@gmail.com>
2020-05-08 18:43:02 +0700

    Code-style

--- src/console.h
+++ src/console.h
@@ -6,6 +6,7 @@ int swap( char *a, char *b );
 int eat( char *a, char *b );
 int f(char char_number);
 int check_hod(char a[][M], int N, int M, char *hod);
-int hod_p( char a[][M], int i, int j, int k, int l, int f);
+int hod_pw( char a[][M], int i, int j, int k, int l, int f);
+int hod_pb( char a[][M], int i, int j, int k, int l, int f); 
 int figurs (char a[N][M], int i, int j, int k, int l, int f);
  #endif
