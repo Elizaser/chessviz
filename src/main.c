7ec99b9c7c8e67815fca19b51ee560088263acdc
elizaser <liza.gluhowrowa@gmail.com>
2020-05-09 17:30:20 +0700

    Code-style

--- src/main.c
+++ src/main.c
@@ -1,27 +1,26 @@
+#include "console.h"
 #include <stdio.h>
 #include <string.h>
-#include "console.h"
 const int N = 8, M = 9;
 
-int main(int argc, char* argv[]) 
+int
+main(int argc, char* argv[])
 {
-    FILE* file;
-    if (!(file = fopen(argv[1], "r"))) 
-    {
-      printf("ERROR INPUT FILE\n");
-      return -1;
-    }
+  FILE* file;
+  if (!(file = fopen(argv[1], "r"))) {
+    printf("ERROR INPUT FILE\n");
+    return -1;
+  }
 
-    char a[N][M];
-    char buffer[12];
-    int i = 0;
-    while (fgets(buffer, 12, file)) 
-    {
-      strcpy(a[i], buffer);
-      i++;
-    }
+  char a[N][M];
+  char buffer[12];
+  int i = 0;
+  while (fgets(buffer, 12, file)) {
+    strcpy(a[i], buffer);
+    i++;
+  }
 
-    fclose(file);
-    console(a, N, M);
-    return 0;
+  fclose(file);
+  console(a, N, M);
+  return 0;
 }
--- src/printb.h
+++ src/printb.h
@@ -2,5 +2,6 @@
 #define ADD_H
 #include <stdio.h>
 extern const int N, M;
-void printb(char a[][M], int N, int M);
+void
+printb(char a[][M], int N, int M);
 #endif
