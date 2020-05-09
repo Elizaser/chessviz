697633c4c600b7831771f816ab408af1e863fec7
elizaser <liza.gluhowrowa@gmail.com>
2020-05-09 16:05:17 +0700

    Fixed the library

--- src/console.h
+++ src/console.h
@@ -1,12 +1,12 @@
 #ifndef ADD_H
 #define ADD_H 	
 extern  const int N, M;
-int console(char a[N][M], int N, int M);
 int swap( char *a, char *b );
-int eat( char *a, char *b );
 int f(char char_number);
+int eat( char *a, char *b );
 int check_hod(char a[][M], int N, int M, char *hod);
-int hod_pw( char a[][M], int i, int j, int k, int l, int f);
-int hod_pb( char a[][M], int i, int j, int k, int l, int f); 
-int figurs (char a[N][M], int i, int j, int k, int l, int f);
+int hod_pw(char a[][M], int i, int j, int k, int l);
+int hod_pb(char a[][M], int i, int j, int k, int l); 
+int hod_l(char a[N][M], int i, int j, int k, int l);
+int console(char a[N][M], int N, int M);
  #endif
