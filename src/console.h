fdd58128ed27c348ec04b09676866eb584a3ed33
elizaser <liza.gluhowrowa@gmail.com>
2020-05-09 17:34:34 +0700

    Code-style

--- src/console.h
+++ src/console.h
@@ -1,18 +1,11 @@
 #ifndef ADD_H
 #define ADD_H
 extern const int N, M;
-int
-swap(char* a, char* b);
-int
-f(char char_number);
-int
-check_hod(char a[][M], int N, int M, char* hod);
-int
-hod_pw(char a[][M], int i, int j, int k, int l);
-int
-hod_pb(char a[][M], int i, int j, int k, int l);
-int
-hod_l(char a[N][M], int i, int j, int k, int l);
-int
-console(char a[N][M], int N, int M);
+int swap(char* a, char* b);
+int f(char char_number);
+int check_hod(char a[][M], int N, int M, char* hod);
+int hod_pw(char a[][M], int i, int j, int k, int l);
+int hod_pb(char a[][M], int i, int j, int k, int l);
+int hod_l(char a[N][M], int i, int j, int k, int l);
+int console(char a[N][M], int N, int M);
 #endif
