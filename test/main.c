83dcac09580a919fa9e31a7574a1d717ef3681d1
elizaser <liza.gluhowrowa@gmail.com>
2020-06-01 14:32:50 +0700

    Wrote the first simple test

--- test/main.c
+++ test/main.c
@@ -1,8 +1,31 @@
 #define CTEST_MAIN
+#include "console.h"
 #include <ctest.h>
+const int N = 8, M = 9;
 
-int main ( int argc, const char** argv)
+CTEST(B_Peshka, Correct_p_forward)
 {
-	int test_result = ctest_main(argc, argv);
-	return test_result; 
+    char move[6], a[N][M];
+    move[0] = 'P';
+    move[1] = 'e';
+    move[2] = '7';
+    move[3] = '-';
+    move[4] = 'e';
+    move[5] = '6';
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            a[i][j] = ' ';
+        }
+    }
+    a[6][4] = 'P';
+    a[5][4] = ' ';
+    int result = move_p_b(a, move, 6, 4, 5, 4);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+int main(int argc, const char** argv)
+{
+    int test_result = ctest_main(argc, argv);
+    return test_result;
 }
