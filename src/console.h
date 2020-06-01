89389398e318ae5d7ebd18793cd107eed24ae143
elizaser <liza.gluhowrowa@gmail.com>
2020-06-01 22:09:14 +0700

    Code-style10

--- src/console.h
+++ src/console.h
@@ -1,12 +1,12 @@
 #ifndef ADD_H
 #define ADD_H
 extern const int N, M;
-void swap(char* a, char* b);
-void eat(char* a, char* b);
+void swap(char *a, char *b);
+void eat(char *a, char *b);
 int f(char char_number);
 int check_place_r_q_p_k(char a[][M], int i, int j, int k, int l);
 int check_place_b_q_k_p(char a[][M], int i, int j, int k, int l);
-int check_move(char a[][M], char* move);
+int check_move(char a[][M], char *move);
 int move_p_w(char a[][M], char move[3], int i, int j, int k, int l);
 int move_p_b(char a[][M], char move[3], int i, int j, int k, int l);
 int move_r_w(char a[N][M], char move[3], int i, int j, int k, int l);
--- src/printb.c
+++ src/printb.c
@@ -1,15 +1,14 @@
 #include "printb.h"
 #include <stdio.h>
 extern const int N, M;
-void printb(char a[][M])
-{
-    int i, j;
-    printf("   a b c d e f g h\n");
-    for (i = 0; i < N; i++) {
-        printf("\n%d ", i + 1);
-        for (j = 0; j < M; j++) {
-            printf("%2c", a[i][j]);
-        }
+void printb(char a[][M]) {
+  int i, j;
+  printf("   a b c d e f g h\n");
+  for (i = 0; i < N; i++) {
+    printf("\n%d ", i + 1);
+    for (j = 0; j < M; j++) {
+      printf("%2c", a[i][j]);
     }
-    printf("\n");
+  }
+  printf("\n");
 }
